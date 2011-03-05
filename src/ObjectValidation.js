4df9aeba1ecb6cd32ac27534ba133ba3d8841405
miko <milosz.kordecki@gmail.com>
2011-03-05 21:02:55 +0100

    ObjectValidation, use

---
+++ src/ObjectValidation.js
@@ -0,0 +1,78 @@
+cc.ObjectValidation = {
+  'private': {}
+};
+
+cc.ObjectValidation.private.AbstractValidator = cc.class(function() {
+  this.isValid = function(value) {
+    return this.isOptional(value);
+  }
+  this.isOptional = function(value) {
+    return false;
+  }
+});
+
+cc.ObjectValidation.private.SchemeValidator = cc.class(function() {
+  this.isValid = function(scheme) {
+    var valid = true;
+    for(var key in scheme) {
+      valid = valid && scheme[key] instanceof cc.ObjectValidation.private.AbstractValidator;
+    }
+    return valid;
+  }
+});
+
+cc.ObjectValidation.private.Optional = {
+  isOptional: function(value) {
+     return null === value || 'undefined' === typeof(value);
+  }
+}
+
+cc.ObjectValidation.IntValidator = cc.class(function() {
+  this.isValid = function(value) {
+    return this.parent('isValid', value) || 'number' === typeof(value);
+  }
+}).cc.inherits(cc.ObjectValidation.private.AbstractValidator);
+
+cc.ObjectValidation.StringValidator = cc.class(function() {
+  this.isValid = function(value) {
+    return this.parent('isValid', value) || 'string' === typeof(value);
+  }
+}).cc.inherits(cc.ObjectValidation.private.AbstractValidator);
+
+cc.ObjectValidation.OptionalStringValidator = cc.class(function() {
+}).cc.inherits(cc.ObjectValidation.StringValidator).cc.implements(cc.ObjectValidation.private.Optional);
+
+cc.ObjectValidation.ObjectValidator = cc.class(function() {
+  this.isValid = function(value) {
+    return this.parent('isValid', value) || 'object' === typeof(value);
+  }
+}).cc.inherits(cc.ObjectValidation.private.AbstractValidator);
+
+cc.ObjectValidation.ArrayValidator = cc.class(function() {
+  this.isValid = function(value) {
+    return this.parent('isValid', value) && Array == value.constructor;
+  }
+}).cc.inherits(cc.ObjectValidation.ObjectValidator);
+
+
+cc.ObjectValidation.ObjectSchemeValidator = cc.class(function(scheme) {
+  if(!new cc.ObjectValidation.private.SchemeValidator().isValid(scheme)) {
+    throw 'scheme is invalid';
+  }
+  this.scheme = scheme;
+  this.isValid = function(obj) {
+    if(!this.parent('isValid',obj)){
+      return false;
+    }
+    for(var key in obj) {
+      if(this.scheme[key] === undefined) {
+        return false;
+      }
+    }
+    var valid = true;
+    for(var key in this.scheme) {
+      valid = valid && this.scheme[key].isValid(obj[key]);
+    }
+    return valid;
+  }
+}).cc.inherits(cc.ObjectValidation.ObjectValidator);
--- src/cc.js
+++ src/cc.js
@@ -2,30 +2,46 @@ var cc = {
   'private' : {}
 };
 
-cc.private.abstractValidator = function() {
-  this.optional = false;
-  this.isValid = function(value) {
-    throw 'abstract';
-  }
-  this.parent = function() {
-    return new this.constructor;
-  }
-  this.is_optional = function(value) {
-    return null === value || 'undefined' === typeof(value);
-  }
-};
-
-cc.private.schemeValidator = function() {
-  this.isValid = function(scheme) {
-    var valid = true;
-    for(var key in scheme) {
-      valid = valid && scheme[key] instanceof cc.private.abstractValidator;
+cc.class = function(func) {
+  func.cc = {};
+  func.prototype = new func;
+  func.prototype.constructor = func;
+  func.prototype.parent = func.prototype;
+  func.prototype.parent = function(funcName) {
+    return this.parentClass[funcName].apply(this, Array.prototype.slice.apply(arguments, [1]));
+  };
+  func.cc.inherits = function(parent) {
+    if(!parent instanceof Function) {
+      throw 'function expected';
     }
-    return valid;
+    func.prototype = new parent;
+    func.prototype.constructor = func;
+    func.prototype.parentClass = parent.prototype;
+    return func;
+  }
+  func.cc.implements = function(object) {
+    if(!object instanceof Object) {
+      throw 'object expected';
+    }
+    for(var key in object) {
+      func.prototype[key] = object[key]
+    }
+    return func;
+  }
+  return func;
+}
+
+cc.use = function(module, exclude) {
+  exclude = exclude || /^private$/;
+  var ref = window;
+  for(var funcName in module) {
+    if(!funcName.match(exclude)) {
+      window[funcName] = module[funcName];
+    }
   }
 }
 
-cc.private.get_object_length = function(obj) {
+cc.private.getObjectLength = function(obj) {
   if(!(obj instanceof Object)) {
     throw 'is not an object';
   }
@@ -35,60 +51,3 @@ cc.private.get_object_length = function(obj) {
   }
   return i;
 }
-
-cc.intValidator = function() {
-  this.isValid = function(value) {
-    return 'number' === typeof(value);
-  }
-};
-cc.intValidator.prototype = new cc.private.abstractValidator;
-
-cc.stringValidator = function() {
-  this.isValid = function(value) {
-    return 'string' === typeof(value);
-  }
-}
-cc.stringValidator.prototype = new cc.private.abstractValidator;
-
-cc.objectValidator = function() {
-  this.isValid = function(value) {
-    return 'object' === typeof(value);
-  }
-}
-cc.objectValidator.prototype = new cc.private.abstractValidator;
-
-cc.arrayValidator = function() {
-  this.isValid = function(value) {
-    return this.parent().isValid(value) && Array == value.constructor;
-  }
-}
-cc.arrayValidator.prototype = new cc.objectValidator;
-cc.arrayValidator.prototype.constructor = cc.objectValidator;
-
-cc.optionalStringValidator = function() {
-  this.isValid = function(value) {
-    return this.is_optional(value) || this.parent().isValid(value);
-  }
-}
-cc.optionalStringValidator.prototype = new cc.stringValidator;
-cc.optionalStringValidator.prototype.constructor = cc.stringValidator;
-
-cc.objectSchemeValidator = function(scheme) {
-  if(!new cc.private.schemeValidator().isValid(scheme)) {
-    throw 'scheme is invalid';
-  }
-  this.scheme = scheme;
-  this.isValid = function(obj) {
-    for(var key in obj) {
-      if(this.scheme[key] === undefined) {
-        return false;
-      }
-    }
-    var valid = true;
-    for(var key in this.scheme) {
-      valid = valid && this.scheme[key].isValid(obj[key]);
-    }
-    return valid;
-  }
-}
-cc.objectSchemeValidator.prototype = new cc.private.abstractValidator;
